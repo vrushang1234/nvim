66b71d589b6cc51173813e4651717484542d3a9f
vrushang1234 <vrushanganand20@gmail.com>
2025-12-20 01:48:34 -0800

    Fixed issues with lsp and treesitter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,71 +1,49 @@
 return {
-	-- Mason: LSP/DAP/Formatter installer
-	{
-  "mason-org/mason-lspconfig.nvim",
-  dependencies = { "mason-org/mason.nvim" },
-  opts = {
-    ensure_installed = { "pyright", "ts_ls", "lua_ls", "clangd", "rust_analyzer", "cssls", "html" },
-    -- If you prefer auto-enabling, set this to true (default) and
-    -- you can remove the vim.lsp.enable loop above.
-    automatic_enable = false,
-  },
-},
-
-	-- Mason-LSPConfig: bridges mason and nvim-lspconfig
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "mason-org/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"pyright",
-					"ts_ls",
-					"lua_ls",
-					"clangd",
-					"rust_analyzer",
-					"cssls",
-					"html",
-				},
-				automatic_enable = false,
-			})
-		end,
-	},
-
-	-- LSPConfig: actual LSP client configuration
-	{
-  "neovim/nvim-lspconfig",
-  dependencies = {
+  {
     "mason-org/mason.nvim",
-    "mason-org/mason-lspconfig.nvim",
+    build = ":MasonUpdate",
+    config = true,
   },
-  config = function()
-    -- Diagnostics UI
-    vim.diagnostic.config({
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      float = { border = "rounded", source = "always" },
-    })
 
-    -- Capabilities for nvim-cmp, apply to ALL servers
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    vim.lsp.config("*", { capabilities = capabilities })  -- default for every server (0.11+)
-
-    -- Per-server settings (only when you need them)
-    vim.lsp.config("lua_ls", {
-      settings = {
-        Lua = {
-          diagnostics = { globals = { "vim" } },
-          workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
-          telemetry = { enable = false },
-        },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "pyright",
+        "ts_ls",
+        "lua_ls",
+        "clangd",
+        "rust_analyzer",
+        "cssls",
+        "html",
       },
-    })
+      automatic_installation = false,
+    },
+  },
 
-    -- If you keep auto-enable OFF below, enable explicitly:
-    for _, server in ipairs({ "pyright", "ts_ls", "lua_ls", "clangd", "rust_analyzer", "cssls", "html" }) do
-      vim.lsp.enable(server)
-    end
-  end,
-},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config("*", { capabilities = capabilities })
+
+      for _, server in ipairs({
+        "pyright",
+        "ts_ls",
+        "lua_ls",
+        "clangd",
+        "rust_analyzer",
+        "cssls",
+        "html",
+      }) do
+        vim.lsp.enable(server)
+      end
+    end,
+  },
 }
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
--- lua/plugins/treesitter.lua
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"python",
-				"c",
-				"cpp",
-				"javascript",
-				"typescript",
-				"rust",
-				"go",
-				"html",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = "<S-CR>",
-					node_decremental = "<BS>",
-				},
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "python",
+      "c",
+      "cpp",
+      "javascript",
+      "typescript",
+      "rust",
+      "go",
+    },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        scope_incremental = "<S-CR>",
+        node_decremental = "<BS>",
+      },
+    },
+  },
 }
+

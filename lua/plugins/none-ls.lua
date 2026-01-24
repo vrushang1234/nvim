17770bd13316acee020da35c64a86b9e4d847514
vrushang1234 <vrushanganand20@gmail.com>
2026-01-23 16:41:52 -0800

    Added rust-analyzer for rust formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,37 +1,49 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		event = "BufReadPre",
-		dependencies = { "nvimtools/none-ls-extras.nvim" },
-		config = function()
-			local null_ls = require("null-ls")
+  {
+    "nvimtools/none-ls.nvim",
+    event = "BufReadPre",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    null_ls.builtins.formatting.clang_format,
-					null_ls.builtins.completion.spell,
-					require("none-ls.diagnostics.eslint"),
-				},
-        capabilities = {
-          offsetEncoding = {"utf-8"},
-        }
-			})
+      null_ls.setup({
+        sources = {
+          -- Lua
+          null_ls.builtins.formatting.stylua,
 
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				callback = function(args)
-					vim.lsp.buf.format({
-						bufnr = args.buf,
-						filter = function(client)
-							return client.name == "null-ls"
-						end,
-						timeout_ms = 2000,
-					})
-				end,
-			})
-		end,
-	},
+          -- JS / TS / Web
+          null_ls.builtins.formatting.prettier,
+
+          -- Python
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.isort,
+
+          -- C / C++
+          null_ls.builtins.formatting.clang_format,
+
+          -- Misc
+          null_ls.builtins.completion.spell,
+
+          -- ESLint diagnostics
+          require("none-ls.diagnostics.eslint"),
+        },
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function(args)
+          vim.lsp.buf.format({
+            bufnr = args.buf,
+            filter = function(client)
+              return client.name == "null-ls"
+                or client.name == "rust_analyzer"
+            end,
+            timeout_ms = 2000,
+          })
+        end,
+      })
+    end,
+  },
 }
+

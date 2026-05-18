7fd0efe1163a105fe5d281d0d2ffae6e04f87c69
vrushang1234 <vrushanganand20@gmail.com>
2026-05-17 17:14:02 -0700

    Add keymappings for moving lines and updated packages

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,14 +8,21 @@ return {
     { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find Buffers" },
   },
   config = function()
-    require("telescope").setup({
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
+   require("telescope").setup({
+  defaults = {
+    layout_strategy = "vertical",
+    sorting_strategy = "ascending",
+    layout_config = {
+      vertical = {
+        width = 0.9,
+        height = 0.95,
+        preview_height = 0.6,
+        prompt_position = "top",
       },
-    })
+    },
+    winblend = 0,
+  },
+})
    local hl = vim.api.nvim_set_hl
     local groups = {
       "TelescopeNormal",

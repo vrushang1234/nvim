74c0388782ebf7a0c4ba67dbb732e6311784f61c
vrushang1234 <vrushanganand20@gmail.com>
2026-07-20 08:59:52 -0700

    Fixed some smear-cursor changes

--- lua/plugins/smear_cursor.lua
+++ lua/plugins/smear_cursor.lua
@@ -3,22 +3,23 @@ return {
     "https://github.com/sphamba/smear-cursor.nvim",
     config = function()
       require("smear_cursor").setup({
-        -- enabled = false,
         cursor_color = "#ffffff",
 
         never_draw_over_target = true,
 
         smear_insert_mode = false,
-        min_vertical_distance_smear = 2,
-        min_horizontal_distance_smear = 2,
+        min_vertical_distance_smear = 3,
+        min_horizontal_distance_smear = 4,
 
-        time_interval = 17, -- ms
-        stiffness = 0.9,
-        trailing_stiffness = 0.4,
-        damping = 0.99, -- stops bouncing
+        time_interval = 16,
 
-        -- anticipation = 0.2,
-        -- distance_stop_animating = 0.6,
+        stiffness = 0.7,
+        trailing_stiffness = 0.65,
+        damping = 0.9,
+
+        distance_stop_animating = 0.6,
+        max_length = 12,
+        smear_between_buffers = false
       })
     end,
   },
